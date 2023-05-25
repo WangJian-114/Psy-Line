a0bd07acaaa73b5357c8acefc1ee2396cee4f77c
golivertuc <56194806+golivertuc@users.noreply.github.com>
2023-05-25 20:00:36 -0300

    pagina de busqueda con estilo
    se agrego algo de estilo a la pagina de busqueda

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'; 
+import './styles/search.css';
 import data from './data.json';
 
 const BuscarPersonas = () => {
@@ -35,48 +36,48 @@ const BuscarPersonas = () => {
   };
 
   return (
-    <div>
-      <h1>Búsqueda de Personas</h1>
+    <div className='seccion-pagina'>
+      <h1>Búsqueda de Psicólogo</h1>
+      <div className='filtro-container'>
+        <div className='filtro-item'>
+          <label>
+            Filtro Consulta:
+            <select value={filtroConsulta} onChange={handleFiltroConsultaChange}>
+              <option value="">Todos</option>
+              <option value="consultaVirtual">Consulta Virtual</option>
+              <option value="consultaPresencial">Consulta Presencial</option>
+              <option value="ambas">Ambas</option>
+            </select>
+          </label>
+        </div>
 
-      <div>
-        <label>
-          Filtro Consulta:
-          <select value={filtroConsulta} onChange={handleFiltroConsultaChange}>
-            <option value="">Todos</option>
-            <option value="consultaVirtual">Consulta Virtual</option>
-            <option value="consultaPresencial">Consulta Presencial</option>
-            <option value="ambas">Ambas</option>
-          </select>
-        </label>
+        <div className='filtro-item'>
+          <label>
+            Filtro Especialidad:
+            <select value={filtroEspecialidad} onChange={handleFiltroEspecialidadChange}>
+              <option value="">Todas</option>
+              <option value="psicoanalisis">Psicoanálisis</option>
+              <option value="terapiaCognitivaConductual">Terapia Cognitiva-Conductual</option>
+              <option value="terapiaFamiliar">Terapia Familiar</option>
+              <option value="terapiaGrupal">Terapia Grupal</option>
+            </select>
+          </label>
+        </div>
+
+        <div className='filtro-item'>
+          <label>
+            Filtro Ubicación:
+            <select value={filtroUbicacion} onChange={handleFiltroUbicacionChange}>
+              <option value="">Todas</option>
+              <option value="Palermo">Palermo</option>
+              <option value="Recoleta">Recoleta</option>
+              <option value="Caballito">Caballito</option>
+            </select>
+          </label>
+        </div>
+
+        <button onClick={buscarPersonas}>Buscar</button>
       </div>
-
-      <div>
-        <label>
-          Filtro Especialidad:
-          <select value={filtroEspecialidad} onChange={handleFiltroEspecialidadChange}>
-            <option value="">Todas</option>
-            <option value="psicoanalisis">Psicoanálisis</option>
-            <option value="terapiaCognitivaConductual">Terapia Cognitiva-Conductual</option>
-            <option value="terapiaFamiliar">Terapia Familiar</option>
-            <option value="terapiaGrupal">Terapia Grupal</option>
-          </select>
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Filtro Ubicación:
-          <select value={filtroUbicacion} onChange={handleFiltroUbicacionChange}>
-            <option value="">Todas</option>
-            <option value="Palermo">Palermo</option>
-            <option value="Recoleta">Recoleta</option>
-            <option value="Caballito">Caballito</option>
-          </select>
-        </label>
-      </div>
-
-      <button onClick={buscarPersonas}>Buscar</button>
-
       <div>
         <h2>Resultados:</h2>
         {resultados.map((persona) => (
@@ -84,7 +85,7 @@ const BuscarPersonas = () => {
             <h3>{persona.nombre} {persona.apellido}</h3>
             <p>Especialidad: {persona.especialidad}</p>
             <p>Descripción: {persona.descripcion}</p>
-            <img src={persona.foto} alt={persona.nombre} className='persona_imagen' />
+            <img src={persona.foto} alt={persona.nombre} className='persona-img' />
           </div>
         ))}
       </div>

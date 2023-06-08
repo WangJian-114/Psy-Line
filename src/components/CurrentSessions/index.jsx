b4f4f427a0ffe46f789c7a338192119d5709f800
iiriina <irinachacon@hotmail.com>
2023-06-08 00:10:39 -0300

    creado el componente sessioncard y pantalla turnos

---
+++ src/components/CurrentSessions/index.jsx
@@ -0,0 +1,37 @@
+import SessionCard from "../SessionCard";
+
+const CurrentSessions = () => {
+
+  return (
+    <div>
+          <div className="div_titulo_turnos">
+            <div className="div_titulo_turnos_interno">
+              <h1 className="titulo_turnos">Mis turnos</h1>
+            </div>
+          </div>
+
+          <div className="div_turnos_confirmados_outline">
+            <div className="div_turnos_confirmados_titulo">
+              <h1 className="turnos_confirmados">Turnos confirmados</h1>
+            </div>
+            {/* aca van a ir los componentes de turnos confirmados, pongo 1 de ejemplo (se llama SessionCard) */}
+            <SessionCard />
+            <SessionCard />
+
+          </div>
+
+        <div className="div_turnos_pendientes_outline">
+          <div className="div_turnos_pendientes_titulo">
+            <h1 className="titulo_turnos_pendientes">Turnos pendientes</h1>
+          </div>
+            {/* aca van a ir los componentes de turnos pendientes, pongo 1 de ejemplo (se llama SessionCard) */}
+            <SessionCard />
+            <SessionCard />
+
+        </div>
+
+    </div>
+  );
+};
+
+export default CurrentSessions;

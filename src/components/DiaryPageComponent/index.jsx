8138b0383300993c0eed8270a8fba93ec3d824af
golivertuc <56194806+golivertuc@users.noreply.github.com>
2023-06-18 21:10:27 -0300

    calendario en castellano
    calendario en castellano

--- src/components/DiaryPageComponent/index.jsx
+++ src/components/DiaryPageComponent/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
-import moment from 'moment';
+import React  from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './styles/DiaryPageComponent.css'
+
+import moment from 'moment';
+import 'moment/locale/es';
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import es from 'date-fns/locale/es';
+
+import './styles/DiaryPageComponent.css';
 
 import { Link, useLocation } from 'react-router-dom';
-
-
-
 
 const CalendarComponent = () => {
 
@@ -35,6 +39,7 @@ const CalendarComponent = () => {
       },
     ];
   
+    
 
     // si entre por primera vez, miAnimo viene en null
     if (miAnimo != null) {
@@ -54,8 +59,18 @@ const CalendarComponent = () => {
       events[2].title = miAnimo;
       }
 
-
-  const localizer = momentLocalizer(moment);
+  // cambio de idioma
+  const locales = {
+    es: es,
+  };
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
+  // fin cambio de idioma
 
   const Event = ({ event }) => {
     return (
@@ -82,12 +97,21 @@ const CalendarComponent = () => {
       <h2 className='texto_elegi_dia_escribir'>Seleccione un día para realizar una entrada en el diario:</h2>
       <div style={{ height: '600px', width: '1200px' }}>
         <Calendar
+         culture="es"
          localizer={localizer}
          events={events}
          startAccessor="start"
          endAccessor="end"
          components={{event:Event, month:{dateHeader: CustomMonthDay}}}
          views={['month']}
+         messages={{
+          next: "Despues",
+          previous: "Atras",
+          today: "Hoy",
+          month: "Mes",
+          week: "Semana",
+          day: "Día"
+         }}
         />
       </div>
     </div>

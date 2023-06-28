97b3cf0da1b4e2dec8407b4395cce101d78c5f4a
iiriina <irinachacon@hotmail.com>
2023-06-28 01:22:43 -0300

    funciona guardar en jardin y se eliminan las act

--- src/components/ActivityForGarden/index.jsx
+++ src/components/ActivityForGarden/index.jsx
@@ -1,48 +1,38 @@
 import React, { useState } from 'react';
 
+const ActivityForGarden = ({ onTaskCompletion, nombreActividad, id }) => {
+  const [isSelected, setIsSelected] = useState(false);
 
-const activityForGarden = ({ onTaskCompletion }) => {
-
-    const [isSelected, setIsSelected] = useState(false);
-
-    const toggleButtonGarden = () => {
+  const toggleButtonGarden = () => {
     setIsSelected(!isSelected);
-    if (!isSelected) {
-        onTaskCompletion(); // Llamada a la función para aumentar el contador de tareas completadas
-      }
-    
+    onTaskCompletion(id, !isSelected ? 1 : -1);
   };
 
+  return (
+    <div>
+      <div className={`frame-13782 ${isSelected ? 'selected' : ''}`}>
+        <div className="nombre-de-tarea-3">{nombreActividad}</div>
+        <button className={`frame-1379 ${isSelected ? 'selected' : ''}`} onClick={toggleButtonGarden}>
+          <svg
+            className="check3"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3.3335 8.66675L6.00016 11.3334L12.6668 4.66675"
+              stroke="#ECF6F6"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-       <div className="frame-13782">
-                <div className="nombre-de-tarea-3">Nombre de tarea</div>
-
-                <button 
-                    className={`frame-1379 ${isSelected ? 'selected' : ''}`}
-                    onClick={toggleButtonGarden}>
-                <svg
-                    className="check3"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                    d="M3.3335 8.66675L6.00016 11.3334L12.6668 4.66675"
-                    stroke="#ECF6F6"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    />
-                </svg>
-                </button>
-            </div>
-
-        </div>
-    );
-}
- 
-export default  activityForGarden;
+export default ActivityForGarden;
--- src/components/GardenDesign/index.jsx
+++ src/components/GardenDesign/index.jsx
@@ -2,66 +2,94 @@ import React, { useState } from 'react';
 import ActivityForGarden from '../ActivityForGarden';
 
 const GardenDesign = ({ ...props }) => {
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const [actividades, setActividadesPendientes] = useState([
+    { id: 1, nombre: "Tarea 1" },
+    { id: 2, nombre: "Tarea 2" },
+    { id: 3, nombre: "Tarea 3" },
+    { id: 4, nombre: "Tarea 4" },
+    { id: 5, nombre: "Tarea 5" },
+    { id: 6, nombre: "Tarea 6" },
+    { id: 7, nombre: "Tarea 7" },
+    { id: 8, nombre: "Tarea 8" },
+    { id: 9, nombre: "Tarea 9" },
+    { id: 10, nombre: "Tarea 10" },
+    { id: 11, nombre: "Tarea 11" },
+    { id: 12, nombre: "Tarea 12" },
+    { id: 13, nombre: "Tarea 13" },
+    { id: 14, nombre: "Tarea 14" }
+  ]);
 
-    //const [isSelected, setIsSelected] = useState(false);
-    const [completedTasks, setCompletedTasks] = useState(0);
-  
-    const handleTaskCompletion = () => {
-        setCompletedTasks(completedTasks + 1);
+  const [actividadesSeleccionadas, setActividadesSeleccionadas] = useState([]);
+  const [actividadesEliminadas, setActividadesEliminadas] = useState([]);
 
-    // const toggleButtonGarden = () => {
-    // setIsSelected(!isSelected);
+  const handleTaskCompletion = (id, isSelected) => {
+    if (isSelected) {
+      setActividadesSeleccionadas((prevActividades) =>
+        prevActividades.concat(id)
+      );
+      setCompletedTasks((prevCompletedTasks) => prevCompletedTasks + 1);
+    } else {
+      setActividadesSeleccionadas((prevActividades) =>
+        prevActividades.filter((actividad) => actividad !== id)
+      );
+      setCompletedTasks((prevCompletedTasks) => prevCompletedTasks - 1);
+    }
   };
 
+  const handleGuardar = () => {
+    const nuevasActividadesPendientes = actividades.filter(
+      (actividad) => !actividadesSeleccionadas.includes(actividad.id)
+    );
+    setActividadesPendientes(nuevasActividadesPendientes);
+    setActividadesEliminadas((prevActividadesEliminadas) =>
+      prevActividadesEliminadas.concat(actividadesSeleccionadas)
+    );
+    setActividadesSeleccionadas([]);
+  };
+
+  const imagen = `${Math.min(actividadesEliminadas.length + 1, 14)}_jardin.png`;
 
   return (
     <div>
-        <div className="jard-n">Jardín</div>
-        <div className="frame-1374">
+      <div className="jard-n">Jardín</div>
+      <div className="frame-1374">
         <div className="frame-1371">
-            <div className="frame-1366">
-  
-            </div>
-
-            <img className="frame-29-1-1" src={`img/${completedTasks}_jardin.png`} />
+          <div className="frame-1366"></div>
+          <img
+            className="frame-29-1-1"
+            src={`img/${imagen}`}
+            alt=""
+          />
         </div>
 
         <div className="frame-1377">
-            <div className="frame-1375">
+          <div className="frame-1375">
             <div className="por-favor-selecciona-las-actividades-que-has-realizado-y-presiona-guardar-para-ver-el-avance-en-el-jard-n">
-                Por favor selecciona las actividades que has realizado y
-                presiona Guardar para ver el avance en el Jardín :)
+              Por favor selecciona las actividades que has realizado y presiona Guardar para ver el avance en el Jardín :)
             </div>
-
             <div className="line-5"></div>
-            </div>
+          </div>
 
-            <div className="frame-1378">
-            
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 1 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 2 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 3 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 4 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 5 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 6 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 7 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 8 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 9 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 10 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 11 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 12 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 13 */}
-            <ActivityForGarden onTaskCompletion={handleTaskCompletion} /> {/* tarea 14 */}
-
-            </div>
+          <div className="frame-1378">
+            {actividades.map((actividad) => (
+              <ActivityForGarden
+                key={actividad.id}
+                nombreActividad={actividad.nombre}
+                id={actividad.id}
+                isSelected={actividadesSeleccionadas.includes(actividad.id)}
+                onTaskCompletion={handleTaskCompletion}
+              />
+            ))}
+          </div>
         </div>
-        </div>
-{/*         <button className="boton_guardar_actividades">
-            <div className="guardar_actividades">Guardar</div>
-        </button> */}
+      </div>
+      <button className="boton_guardar_actividades" onClick={handleGuardar}>
+        <div className="guardar_actividades">Guardar</div>
+      </button>
+      <div className="completed-tasks">Actividades completadas: {completedTasks}</div>
     </div>
   );
+};
 
-}
-
-export default GardenDesign
+export default GardenDesign;
